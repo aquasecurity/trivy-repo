15adb004f636b1b1098cc1d050e0a1a13c049495
Yuval Goldberg <yuvigoldi@gmail.com>
2022-02-16 17:45:56 +0200

    Add GHA to install all the supported Debian/Ubuntu

---
+++ install_deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+set -o xtrace
+
+# Install prerequisites
+apt-get update
+apt-get install -y wget gnupg lsb-release
+
+
+# Install script
+apt-get install apt-transport-https
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list
+apt-get update
+apt-get install -y trivy
+
